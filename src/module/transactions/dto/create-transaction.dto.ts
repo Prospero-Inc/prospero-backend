ac15252ddc58bbea8883e701474662822b07cd56
Natividad <josenatividadcv@gmail.com>
2024-07-31 21:27:04 -0600

    feat: migracion con identificacion de transaction

--- src/module/transactions/dto/create-transaction.dto.ts
+++ src/module/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,23 @@
-export class CreateTransactionDto {}
+import {
+  IsDate,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+} from 'class-validator';
+
+export class CreateTransactionDto {
+  @IsNumber()
+  @IsPositive()
+  amount: number;
+
+  @IsDate()
+  date: Date;
+
+  @IsString()
+  category: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+}
